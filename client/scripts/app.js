562ffc656a6ce07413992649d077673f9fb3dbab
Jeff Chasin <jeff@jeffchasin.com>
2018-07-28 15:48:33 -0400

    implement init and instantiation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,29 @@
 // YOUR CODE HERE:
+const App = function() {
+  this.userName = 'anonymous';
+  this.init();
+};
+
+App.prototype.init = function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  this.userName = urlParams.get('username');
+  console.log('Hello ', this.userName);
+  this.testTemplate();
+};
+
+App.prototype.testTemplate = function() {
+  // grab html string from DOM w template
+
+  // compile template
+
+  // pass object into template, invoking it
+    // set variable to returned html string
+
+  // set html body of chats to html string
+
+};
+
+$(function() {
+  new App();
+
+});
