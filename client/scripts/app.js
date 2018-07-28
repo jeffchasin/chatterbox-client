195f3eb89383cb81ff2187aad3a126f082cb47c3
Jeff Chasin <jeff@jeffchasin.com>
2018-07-28 16:00:40 -0400

    implement handlebars as template engine

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,18 +8,21 @@ App.prototype.init = function() {
   var urlParams = new URLSearchParams(window.location.search);
   this.userName = urlParams.get('username');
   console.log('Hello ', this.userName);
-  this.testTemplate();
+  // this.testTemplate();
 };
 
 App.prototype.testTemplate = function() {
   // grab html string from DOM w template
-
+  var templateString = $('#entry-template').html();
   // compile template
+  var templ = Handlebars.compile(templateString);
 
   // pass object into template, invoking it
-    // set variable to returned html string
+  // set variable to returned html string
+  var renderedTemplate = templ({ username: this.userName, message: 'whatever' });
 
   // set html body of chats to html string
+  $('#chats').html(renderedTemplate);
 
 };
 
