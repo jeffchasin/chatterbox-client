dda225f63bc382ed427d7b3232f3eef725b5f740
Jeff Chasin <jeff@jeffchasin.com>
2018-08-01 00:10:27 -0400

    refactor to last test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,7 @@ const App = function () {
   this.timer = false;
   this.delay = 2000;
   this.currentRoom = 'lobby';
+  this.friends = [];
   this.init();
 };
 
@@ -13,27 +14,33 @@ App.prototype.init = function () {
   var urlParams = new URLSearchParams(window.location.search);
   this.userName = urlParams.get('username');
   console.log('Hello ', this.userName);
+  this.setUpUI();
   // this.testTemplate();
 
   // this.timer = setInterval(() => {
   //   this.fetch();
   // }, this.delay);
-  // this.fetch();
-  // this.setUpUI();
+  this.fetch();
 };
 
 App.prototype.setUpUI = function () {
   const that = this;
-  $('.sendMsgBtn').on('click', function(e) {
-    // grab the msgInput.value
-    const msg = $('.msgInput').val().trim();
-    // invoke postMessage
-    if (msg !== '') {
-      that.send(msg);
-      // clear input box
-      $(this).val('');
-    }
+  $('#send').submit(function (e) {
+    e.preventDefault();
+    that.handleSubmit(e, this);
   });
+};
+
+App.prototype.handleSubmit = function (e, el) {
+  // grab the msgInput.value
+  console.log('this ran');
+  const msg = $('#message').val().trim();
+  // invoke postMessage
+  if (msg !== '') {
+    this.send(msg);
+    // clear input box
+    $(el).val('');
+  }
 };
 
 App.prototype.clearMessages = function () {
@@ -46,15 +53,15 @@ App.prototype.fetch = function () {
     url: this.server,
     method: 'GET',
     dataType: 'json', //could be interpreted as a script
-    success: function(data) {
+    success: function (data) {
       console.log(data);
       that.storeAndDisplayNewMessages(data.results);
     },
-    error: function(error) {
+    error: function (error) {
       console.warn('Server Error: ', error);
     },
-    loading: function() {},
-    complete: function(data) {}
+    loading: function () { },
+    complete: function (data) { }
   });
 };
 
@@ -69,15 +76,26 @@ App.prototype.send = function (msgObj) {
       roomname: msgObj.roomname,
       text: msgObj.text
     }),
-    success: function(data) {
+    success: function (data) {
       console.log(data);
     },
-    error: function(error) {
+    error: function (error) {
       console.warn('Server Error: ', error);
     },
-    loading: function() {},
-    complete: function(data) {}
+    loading: function () { },
+    complete: function (data) { }
   });
+};
+
+App.prototype.handleUsernameClick = function (username) {
+  // append $(this).text() to this.friends
+  if (!this.isFriend(username)) {
+    this.friends.push(username);
+  }
+
+  // search for all messages in the DOM w .username
+  // add .friends to it
+  $('[data-username="' + username + '"]').addClass('friend');
 };
 
 App.prototype.renderRoom = function (roomName) {
@@ -85,7 +103,7 @@ App.prototype.renderRoom = function (roomName) {
 };
 
 App.prototype.storeAndDisplayNewMessages = function (messagesArr) {
-  for (let i=0; i < messagesArr.length; i++) {
+  for (let i = 0; i < messagesArr.length; i++) {
     let objectId = messagesArr[i].objectId;
     if (this.messages[objectId] === undefined) {
       this.messages[objectId] = this.sanitizeMessage(messagesArr[i]);
@@ -96,28 +114,45 @@ App.prototype.storeAndDisplayNewMessages = function (messagesArr) {
 
 App.prototype.sanitizeMessage = function (msg) {
   let attributesToSanitize = ['username', 'roomname', 'text'];
-  for (let i=0; i<attributesToSanitize.length; i++) {
+  for (let i = 0; i < attributesToSanitize.length; i++) {
     let att = attributesToSanitize[i];
     msg[att] = $('<div></div>').text(msg[att]).html();
   }
   return msg;
 };
 
+App.prototype.isFriend = function(username) {
+  return this.friends.indexOf(username) !== -1;
+};
+
 App.prototype.renderMessage = function (msg) {
   // grab html string from DOM w template
-  var templateString = $('#msg-template').html();
+  const that = this;
+  const templateString = $('#msg-template').html();
   // compile template
-  var templ = Handlebars.compile(templateString);
+  const templ = Handlebars.compile(templateString);
   // pass object into template, invoking it
   // set variable to returned html string
-  var renderedTemplate = templ(
+  const renderedTemplate = templ(
     {
       username: msg.username,
       message: msg.text
     });
+  const $renderedTemplate = $(renderedTemplate.trim());
+
+  const $username = $renderedTemplate.find('.username');
+  $username.attr('data-username', msg.username);
+  $username.on('click', function(e) {
+    //this = DOM element
+    that.handleUsernameClick(msg.username);
+  });
+
+  if (this.isFriend(msg.username)) {
+    $renderedTemplate.addClass('friend');
+  }
 
   // set html body of chats to html string
-  $('#chats').prepend(renderedTemplate);
+  $('#chats').prepend($renderedTemplate);
 
 };
 
